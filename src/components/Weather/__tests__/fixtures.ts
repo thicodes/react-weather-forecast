9d2452d8124ee0d7cdcf8a46927425173324ee49
Thiago <thiago.leite@agilize.com.br>
2022-03-26 22:47:10 -0300

    test: Add mock for weather

---
+++ src/components/Weather/__tests__/fixtures.ts
@@ -0,0 +1,35 @@
+export const mockForecast = {
+  properties: {
+    periods: [
+      { number: 1, name: "Sunday", temperature: 10, isDaytime: true },
+      { number: 2, name: "Monday", temperature: 20, isDaytime: true },
+      {
+        number: 3,
+        name: "Tuesday",
+        temperature: 30,
+        isDaytime: true,
+      },
+      {
+        number: 4,
+        name: "Wesdnesday",
+        temperature: 40,
+        isDaytime: true,
+      },
+      {
+        number: 5,
+        name: "Thrusday",
+        temperature: 50,
+        isDaytime: true,
+      },
+      { number: 6, name: "Friday", temperature: 60, isDaytime: true },
+      {
+        number: 7,
+        name: "Saturday",
+        temperature: 70,
+        isDaytime: true,
+      },
+      { number: 8, name: "Sunday", temperature: 80, isDaytime: true },
+      { number: 9, name: "Monday", temperature: 90, isDaytime: true },
+    ],
+  },
+};
