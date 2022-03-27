4058e067ce289ea1d8a206480a1c175cd1c59afe
Thiago <thiago.leite@agilize.com.br>
2022-03-26 22:46:10 -0300

    feat: Add proxy call using express

---
+++ api/index.js
@@ -0,0 +1,15 @@
+const app = require("express")();
+const fetch = require("node-fetch");
+
+app.get("/api/search-geocode/:address", async (req, res) => {
+  try {
+    const response = await fetch(
+      `https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address=${req.params.address}&benchmark=2020&format=json`
+    ).then(async (response) => await response.json());
+    res.status(200).json(response);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+app.listen(4000);
